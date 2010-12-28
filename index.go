7fc63a507fc1062d0ae5b6379230f8a065a403ca
Taru Karttunen <taruti@taruti.net>
2010-12-28 19:35:35 +0200

    Formatting

--- index.go
+++ index.go
@@ -3,8 +3,7 @@ package main
 import (
 	"bitbucket.org/taruti/taglib.go"
 	"go9p.googlecode.com/hg/p/srv"
-
-	)
+)
 
 type AudioFile struct {
 	taglib.Tags
@@ -15,31 +14,36 @@ type AudioFile struct {
 type Query func(Map)
 type Index chan Query
 
-
 func indexLoop(ch chan Query, m Map) {
 	for {
-		f :=<- ch
-		if nil==f { return }
+		f := <-ch
+		if nil == f {
+			return
+		}
 		f(m)
 	}
 }
 
 func spawnIndexLoop(tree Map) Index {
 	ch := make(chan Query, 1)
-	go indexLoop(ch,tree)
+	go indexLoop(ch, tree)
 	return Index(ch)
 }
 
 func buildAudioIndexes() {
-	if indexes == nil { indexes = &map[string]Index{} }
+	if indexes == nil {
+		indexes = &map[string]Index{}
+	}
 
 	(*indexes)["Artist"] = spawnIndexLoop(Map{})
 }
 
 func audioAddToIndexes(af *AudioFile) {
 	(*indexes)["Artist"] <- func(m Map) {
-		if af.Artist == "" { af.Artist = "@" }
-		addToMap(af.Artist, af, m) 
+		if af.Artist == "" {
+			af.Artist = "@"
+		}
+		addToMap(af.Artist, af, m)
 	}
 }
 
