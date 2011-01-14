d2998723f402891be30c952d4543b5586ded4703
Taru Karttunen <taruti@taruti.net>
2011-01-14 17:57:07 +0200

    Add Search function

--- http.go
+++ http.go
@@ -2,50 +2,45 @@ package main
 
 import (
 	"bitbucket.org/taruti/http_jsonrpc.go"
+	"bitbucket.org/taruti/taglib.go"
     "http"
-    "io/ioutil"
     "os"
+	"regexp"
 )
 
-type page struct {
-    title string
-    body  []byte
+type afhtml struct {
+	taglib.Tags
+	Id uint64
 }
 
-func (p *page) save() os.Error {
-    filename := p.title + ".txt"
-    return ioutil.WriteFile(filename, p.body, 0600)
-}
-
-func loadPage(title string) (*page, os.Error) {
-    filename := "test.html"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &page{title: title, body: body}, nil
-}
-
-const lenPath = len("/view/")
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[lenPath:]
-    p, _ := loadPage(title)
-    w.Write(p.body)
-	switch r.Method {
-	case "GET":
-	case "POST":
-	default:
+func Search(regex string) ([]afhtml,os.Error) {
+	re,err := regexp.Compile(regex)
+	if err!=nil {
+		return nil,err
 	}
+	ch := make(chan []afhtml)
+	(*indexes)["Artist"] <- func(mr Map) {
+		res := []afhtml{}
+		m := mr.(MapSM)
+		for artist,afs := range m {
+			if re.MatchString(artist) {
+				for i:=0; i<len(afs); i++ {
+					res = append(res, afhtml{afs[i].Tags, afs[i].Path})
+				}
+			}
+		}
+		ch <- res
+	}
+	return <-ch,nil
 }
 
 func httpmain() {
 	s := http_jsonrpc.New()
+	s.Register("search", Search)
 	s.Register("play", PlayId)
 	s.Register("stop", Stop)
 	s.Register("volume", VolumeModPercent)
 	http.Handle("/post", s)
 	http.Handle("/", http.FileServer("static", ""))
-	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
